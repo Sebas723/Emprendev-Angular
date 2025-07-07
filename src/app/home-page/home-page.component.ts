f913f1050ad40e034319672e7efe6cf60f7b1954
sebas723 <tairon723@gmail.com>
2025-07-07 13:50:34 -0500

    first draft of the about us section finished

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { AboutUsSectionComponent } from './components/about-us-section/about-us-section.component';
 
 @Component({
   selector: 'app-home-page',
   imports: [
     NavbarComponent,
-    HeroSectionComponent
+    HeroSectionComponent,
+    AboutUsSectionComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
