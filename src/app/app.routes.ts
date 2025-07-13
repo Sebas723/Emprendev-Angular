11415f6d33fc5c112b5956dfdfec4683c6f8de48
sebas723 <tairon723@gmail.com>
2025-07-13 14:11:42 -0500

    Creating the components of the registration form and starting the design, the final style still needs to be defined

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
-import { Component } from '@angular/core';
 import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 export const routes: Routes = [
     {
         path: 'home',
         component: HomePageComponent,
+    },
+    {
+        path: 'register',
+        component: RegisterFormComponent,
     },
     {
         path: '**',
