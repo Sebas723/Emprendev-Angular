bfac999c8a6091c9c863d48d71adb6d0a5dec1bc
sebas723 <tairon723@gmail.com>
2025-07-24 12:27:12 -0500

    making a little modal window to developer and offer cards

--- src/app/catalogs-section-demo/catalog-devs/catalog-devs.component.ts
+++ src/app/catalogs-section-demo/catalog-devs/catalog-devs.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-catalog-devs',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './catalog-devs.component.html',
   styleUrl: './catalog-devs.component.css'
 })
 export class CatalogDevsComponent {
-
+  
+  isSidebarOpen: boolean = false;
 }
